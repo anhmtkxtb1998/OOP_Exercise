9948f9b7d42f2ae91bce40b65b1b53d5571c6cb2
kudos <54769903+qq17@users.noreply.github.com>
2021-02-24 23:35:38 +0300

    final

--- OOP_Exercise/application.cpp
+++ OOP_Exercise/application.cpp
@@ -10,7 +10,7 @@ TApplication::TApplication()
 int TApplication::exec()
 {
     int ch;
-    number a = 1,b =1,c=1;
+    number a = 1, b =2, c=1;
     while(true)
     {
         ch = menu();
@@ -19,12 +19,12 @@ int TApplication::exec()
         case 1:
             do
             {
-                std::cout<< "a = ";
-                std::cin >>a;
-                std::cout <<"b = ";
-                std::cin >>b;
-                std::cout <<"c = ";
-                std::cin >>c;
+                std::cout << "a = ";
+                std::cin >> a;
+                std::cout << "b = ";
+                std::cin >> b;
+                std::cout << "c = ";
+                std::cin >> c;
 
                 if (a == 0)
                     std::cout << "Value of 'a' cant be 0! Reenter coefficients!" << std::endl;
@@ -33,17 +33,17 @@ int TApplication::exec()
 
         case 2:
         {
-            Polinom p (a,b,c);
+            Polinom p (a, b, c);
             number x;
             std::cout <<"x = ";
-            std::cin >>x;
-            std::cout <<"p(x) = " << p.value(x) << std::endl;
+            std::cin >> x;
+            std::cout << "p(x) = " << p.value(x) << std::endl;
         }
             break;
 
         case 3:
         {
-            Polinom p(a,b,c);
+            Polinom p(a, b, c);
             number* roots = p.roots();
             if (roots == NULL)
                 std::cout << "The equation has no solutions!" << std::endl;
@@ -52,23 +52,23 @@ int TApplication::exec()
                 if (roots[0] == roots[1])
                     std::cout << "The equation has one solution: x1 = " << roots[0] << std::endl;
                 else
-                    std::cout << "The equation has 2 solutions: x1 = " << roots[0] <<", x2 = " << roots[1] << std::endl;
+                    std::cout << "The equation has two solutions: x1 = " << roots[0] << ", x2 = " << roots[1] << std::endl;
             }
         }
             break;
 
         case 4:
         {
-            Polinom p(a,b,c);
-            p.SetPrintMode(EPrintModeClassic);
+            Polinom p(a, b, c);
+            p.setPrintMode(EPrintModeClassic);
             std::cout << p << std::endl;
         }
             break;
 
         case 5:
         {
-            Polinom p(a,b,c);
-            p.SetPrintMode(EPrintModeCanonic);
+            Polinom p(a, b, c);
+            p.setPrintMode(EPrintModeCanonic);
             std::cout << p << std::endl;
         }
             break;
@@ -87,11 +87,11 @@ int TApplication::menu()
 {
     int ch;
     std::cout << "0 - exit" << std::endl;
-    std::cout << "1 - coefficients" << std::endl;
-    std::cout << "2 - value" << std::endl;
-    std::cout << "3 - roots" << std::endl;
-    std::cout << "4 - print(classic)" << std::endl;
-    std::cout << "5 - print(canonic)" << std::endl << "> ";
-    std::cin >>ch;
+    std::cout << "1 - enter coefficients" << std::endl;
+    std::cout << "2 - calculate value" << std::endl;
+    std::cout << "3 - find roots" << std::endl;
+    std::cout << "4 - print polinom (classic form)" << std::endl;
+    std::cout << "5 - print polinom (canonic form)" << std::endl << "> ";
+    std::cin >> ch;
     return ch;
 }
--- OOP_Exercise/polinom.cpp
+++ OOP_Exercise/polinom.cpp
@@ -1,33 +1,33 @@
 #include "polinom.h"
-#include<cmath>
+#include <cmath>
 
-Polinom::Polinom(number x,number y,number z)
+Polinom::Polinom(number a, number b, number c)
 {
     printmode = EPrintModeClassic;
-    a = x;
-    b = y;
-    c = z;
+    this->a = a;
+    this->b = b;
+    this->c = c;
 }
 
-void Polinom::SetPrintMode(EPrintMode mode)
+void Polinom::setPrintMode(EPrintMode mode)
 {
     printmode = mode;
 }
 
 number Polinom::value(number x)
 {
-    return (a * x *x  + b * x + c);
+    return (a * x * x + b * x + c);
 }
 
 number * Polinom::roots()
 {
-    number delta = b * b - 4 * a * c;
-    if (delta >= 0)
+    number discr = b * b - 4 * a * c;
+    if (discr >= 0)
     {
         number * xroots = new number[2];
-        xroots[0] = (-b - sqrt(delta))/(2 * a);
-        xroots[1] = (-b + sqrt(delta))/(2 * a);
-        if (-a*(xroots[0] + xroots[1]) == b && a*xroots[0]*xroots[1] == c)
+        xroots[0] = (-b - sqrt(discr))/(2 * a);
+        xroots[1] = (-b + sqrt(discr))/(2 * a);
+        if (-a * (xroots[0] + xroots[1]) == b && a * xroots[0] * xroots[1] == c)
         {
             return xroots;
         }
@@ -49,19 +49,18 @@ number * Polinom::roots()
 std::ostream& operator <<(std::ostream& os,Polinom& p)
 {
     if (p.printmode == EPrintModeClassic)
-        os << "p(x) = " << p.a <<"x^2 " << (p.b < 0 ? "" : "+") << p.b << "x " << (p.c < 0 ? "" : "+") << p.c <<std::endl;
+        os << "p(x) = " << p.a << "x^2 " << (p.b < 0 ? "- " : "+ ") << std::abs(p.b) << "x " << (p.c < 0 ? "- " : "+ ") << std::abs(p.c) << std::endl;
     else
     {
-        if (p.b % (2*p.a) == 0)
+        number x0 = -p.b/(2*p.a);
+        if (x0 * 2 * p.a == -p.b)
         {
-            number x0 = -p.b/(2*p.a);
             number y0 = p.value(x0);
-
-            os << p.a << "(x " << (x0 < 0 ? "+ " : "- ") << std::abs(x0) << ")^2 " << (y0 < 0 ? "" : "+") << y0 << std::endl;
+            os << p.a << "(x " << (x0 < 0 ? "+ " : "- ") << std::abs(x0) << ")^2 " << (y0 < 0 ? "- " : "+ ") << std::abs(y0) << std::endl;
         }
         else
         {
-            os << "Cannot output canonical form because x0 = -b/(2*a) does not belong to type 'number'" << std::endl;
+            os << "Cannot output canonic form because x0 = -b/(2*a) does not belong to type 'number'" << std::endl;
         }
     }
     return os;
